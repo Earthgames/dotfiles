c0b77c8c38f64688c05419076c9bdf6c7160c8c6
Earthgames <earthgame_s@outlook.com>
2024-09-07 03:10:59 +0200

    wayfire: added rofi, removed wofi and wlogout: - rofi replaces wofi and wlogout - added a screenshot menu via rofi

---
+++ rofi/scripts/launcher.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
+#
+## Rofi   : Launcher (Modi Drun, Run, File Browser, Window)
+
+dir="$HOME/.config/rofi"
+theme='launcher'
+
+## Run
+rofi \
+    -show drun -m -1\
+    -theme ${dir}/${theme}.rasi
