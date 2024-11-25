4b8e272ca61dfd862a048bbc10df8467f8213b44
Earthgames <earthgame_s@outlook.com>
2024-11-25 10:31:24 +0100

    nvim: moved git around

--- nvim/lua/plugins/codeCheck.lua
+++ nvim/lua/plugins/codeCheck.lua
@@ -1,14 +1,4 @@
 return {
--- Git
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    config = true
-  },
 -- CodeTree
   {
     "nvim-treesitter/nvim-treesitter",
