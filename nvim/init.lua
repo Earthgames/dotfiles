3e71f7e90eb29a2db9a5c23fa2844a8a67153aa7
earthgame_s@outlook.com <arend.bos@gmail.com>
2025-04-30 17:23:23 +0200

    nvim: moved vimtex config to a different place

--- nvim/init.lua
+++ nvim/init.lua
@@ -240,16 +240,6 @@ vim.cmd([[
     lua require('Comment').setup()
     " Viewer options: One may configure the viewer either by specifying a built-in
     " viewer method:
-    let g:vimtex_view_method = 'zathFormats embedded code blocks - Can format code blocks inside markdown files or similar (see injected language formatting)ura'
-    let g:tex_flavor  = 'latex'
-    let g:tex_conceal = ''
-    let g:vimtex_fold_manual = 1
-    " VimTeX uses latexmk as the default compiler backend. If you use it, which is
-    " strongly recommended, you probably don't need to configure anything. If you
-    " want another compiler backend, you can change it as follows. The list of
-    " supported backends and further explanation is provided in the documentation,
-    " see ":help vimtex-compiler".
-    let g:vimtex_compiler_method = 'tectonic'
 
     " Rainbow brackets vscode theme
     highlight link RainbowDelimiterRed DraculaFg
--- nvim/lua/plugins/laTex.lua
+++ nvim/lua/plugins/laTex.lua
@@ -1,5 +1,21 @@
 return {
-  "lervag/vimtex",
+  {
+    "lervag/vimtex",
+    lazy = false,
+    init = function()
+      vim.g.vimtex_view_method = "zathura"
+      
+      vim.g.tex_flavor  = 'latex'
+      vim.g.tex_conceal = ''
+      -- vim.g:vimtex_fold_manual = 1
+      -- VimTeX uses latexmk as the default compiler backend. If you use it, which is
+      -- strongly recommended, you probably don't need to configure anything. If you
+      -- want another compiler backend, you can change it as follows. The list of
+      -- supported backends and further explanation is provided in the documentation,
+      -- see ":help vimtex-compiler".
+      -- vim.g.vimtex_compiler_method = 'tectonic'
+      end
+  },
   "Konfekt/FastFold",
   "matze/vim-tex-fold",
 }
